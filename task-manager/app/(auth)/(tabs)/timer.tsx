06ac77ea178fc1dd02821e5a3e5a5b4623909472
hazirahi <hazirahi001@mymail.sim.edu.sg>
2024-07-28 01:35:04 +0800

    implemented pomodoro timer

--- task-manager/app/(auth)/(tabs)/timer.tsx
+++ task-manager/app/(auth)/(tabs)/timer.tsx
@@ -1,9 +1,31 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { LinearGradient } from "expo-linear-gradient";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import { useState } from "react";
+
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Timer() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [key, setKey] = useState(0);
+    const [timerMode, setTimerMode] = useState('pomodoro');
+    const [duration, setDuration] = useState(25 * 60);
+
+    const timerModes = {
+        pomodoro: 25 * 60,
+        shortBreak: 5 * 60,
+        longBreak: 15 * 60,
+    };
+
+    const handleTimerMode = (mode: keyof typeof timerModes) => {
+        setTimerMode(mode);
+        setDuration(timerModes[mode]);
+    }
+
+    // add notifs when timer end
+    
     return(
         <SafeAreaView style={styles.container}>
             <LinearGradient 
@@ -13,10 +35,66 @@ export default function Timer() {
             <View style={{paddingHorizontal: 20}}>
                 <Text style={styles.header}>Pomodoro Timer</Text>
             </View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-evenly', paddingTop: 15, paddingHorizontal: 20}}>
+                <TouchableOpacity style={[styles.navBTN, timerMode === 'pomodoro' ? styles.activeNavBTN : null]}
+                    onPress={() => handleTimerMode('pomodoro')}
+                >
+                    <Text style={{fontWeight: '600'}}>Pomodoro</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.navBTN, timerMode === 'shortBreak' ? styles.activeNavBTN : null]}
+                    onPress={() => handleTimerMode('shortBreak')}
+                >
+                    <Text style={{fontWeight: '600'}}>Short Break</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.navBTN, timerMode === 'longBreak' ? styles.activeNavBTN : null]}
+                    onPress={() => handleTimerMode('longBreak')}
+                >
+                    <Text style={{fontWeight: '600'}}>Long Break</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{alignItems: 'center', paddingTop: 40}}>
+                <Text style={{paddingBottom: 30, fontWeight: '500', fontSize: 30}}>Time to Focus!</Text>
+                <CountdownCircleTimer
+                    isPlaying={isPlaying}
+                    duration={duration}
+                    colors={"#A6F511"}
+                    updateInterval={0}
+                    strokeWidth={22}
+                    size={250}
+                    key={key}
+                >
+                {({ remainingTime }) => (
+                    <Text style={{ color: '#D5FF61', fontSize: 50, fontWeight: '700' }}>
+                        {Math.floor(remainingTime / 60).toString().padStart(2, '0')}:
+                        {Math.floor(remainingTime % 60).toString().padStart(2,'0')}
+                    </Text>
+                )}
+                </CountdownCircleTimer>
+                <View style={{flexDirection:'row', paddingTop: 40}}>
+                    
+                    <TouchableOpacity
+                            onPress={() => {
+                                setKey(prevKey => prevKey + 1); 
+                                setIsPlaying(false);
+                            }}
+                    >
+                        <Ionicons name="stop-circle" size={80} color="red" />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() => setIsPlaying(!isPlaying)}
+                    >
+                        {isPlaying ? (
+                            <Ionicons name="pause-circle" size={80} color="#0084FF" />
+                        ) : (
+                            <Ionicons name="play-circle" size={80} color="#0084FF" />
+                        )}
+                        
+                    </TouchableOpacity>
+                </View>
+            </View>
         </SafeAreaView>
     )
-    
-    
 }
 
 const styles = StyleSheet.create({
@@ -37,9 +115,13 @@ const styles = StyleSheet.create({
         fontWeight: '600',
         paddingTop: 10
     },
-    addTaskBTN: {
-        position: 'absolute',
-        bottom: 10,
-        right: 2
+    navBTN: {
+        borderWidth: 1,
+        borderRadius: 20,
+        padding: 10,
+        paddingHorizontal: 15
+    },
+    activeNavBTN: {
+        backgroundColor: '#16B4F8'
     }
 });
